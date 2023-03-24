94995a6c6a9ab6135b801ed30624404cec40b631
SaYMooN <shokoladnysaymoon@mail.ru>
2023-03-25 00:18:53 +0300

    Settings Page visuality is more or less finished

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Linq;
-using System.Resources;
-using System.Text.Json;
+﻿using SaY_DeF.Source;
+using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using SaY_DeF.Source;
 
 namespace SaY_DeF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
-    { 
+    {
         public MainWindow()
         {
             InitializeComponent();
@@ -21,9 +15,15 @@ namespace SaY_DeF
         }
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+            SettinsPage.Height = Height;
+            SettinsPage.Width = Width;
+            MainPage.Height = Height;
+            MainPage.Width = Width;
             LabelReactionToWinSizeChanged();
             PlayButtonReactionToWinSizeChanged();
             SettingsButtonReactionToWinSizeChanged();
+            SettingsPageReaction();
+
         }
         private void LabelReactionToWinSizeChanged()
         {
@@ -40,13 +40,13 @@ namespace SaY_DeF
             Button_Play.BorderThickness = new Thickness(Button_Play.Height / 13 + 2);
 
 
-            PlayTB.Width = Button_Play.Width-Button_Play.Height-10;
-            PlayTB.FontSize = Button_Play.Width /7.8;
+            PlayTB.Width = Button_Play.Width - Button_Play.Height - 10;
+            PlayTB.FontSize = Button_Play.Width / 7.8;
 
             PlayColumnImage.Width = new GridLength(Button_Play.Height);
-            PlayImg.Margin=new Thickness((Button_Play.Width-Button_Play.Width)/220);
-            PlayImg.Height = Button_Play.Height* 0.67;
-            PlayImg.Width = Button_Play.Height* 0.67;
+            PlayImg.Margin = new Thickness((Button_Play.Width - Button_Play.Width) / 220);
+            PlayImg.Height = Button_Play.Height * 0.67;
+            PlayImg.Width = Button_Play.Height * 0.67;
             Canvas.SetLeft(Button_Play, (Width - Button_Play.Width) / 2);
             Canvas.SetTop(Button_Play, (Height - Button_Play.Height) / 3);
         }
@@ -67,7 +67,36 @@ namespace SaY_DeF
             Canvas.SetLeft(Button_Settings, (Width - Button_Play.Width) / 2);
             Canvas.SetTop(Button_Settings, (Height - Button_Play.Height) / 1.82);
         }
-
+        private void SettingsPageReaction()
+        {
+            ReturnButtonReactionToWinSizeChanged();
+            SettingsParametrsReactionToWinSizeChanged();
+        }
+        private void SettingsParametrsReactionToWinSizeChanged()
+        {
+            AllTheSettings.Width = Width *0.9;
+            AllTheSettings.Height = Height * 0.8;
+            foreach (Control item in AllTheSettings.Children)
+            {
+                item.FontSize = Math.Min(Height, Width) / 32+6;
+                if (item is Button)
+                {
+                    item.BorderThickness = new Thickness( Math.Min(AllTheSettings.Height, AllTheSettings.Width) *0.01+2);
+                    item.Width = AllTheSettings.Width / 4;
+                }
+            }
+            Canvas.SetLeft(AllTheSettings, Width*0.05);
+            Canvas.SetTop(AllTheSettings, Height*0.1);
+        }
+        private void ReturnButtonReactionToWinSizeChanged()
+        {
+            Button_Return.Height = Height*0.09;
+            Button_Return.Width=Button_Return.Height*3.2;
+            Button_Return.BorderThickness =new Thickness( Button_Return.Height * 0.12);
+            Button_Return.FontSize = Button_Return.Height/2;
+            Canvas.SetBottom(Button_Return, Button_Return.Height+16);
+            Canvas.SetRight(Button_Return, Button_Return.Height*0.9);
+        }
         private void Button_Play_Click(object sender, RoutedEventArgs e)
         {
             Net_Connector netCon = new Net_Connector();
@@ -81,7 +110,7 @@ namespace SaY_DeF
         {
             MainPage.Visibility = Visibility.Hidden;
             SettinsPage.Visibility = Visibility.Visible;
-            Button_Return.Visibility=Visibility.Visible;
+            Button_Return.Visibility = Visibility.Visible;
             TB_Nick.Text = Settings.myNick;
         }
         private void Button_Return_Click(object sender, RoutedEventArgs e)
@@ -92,10 +121,15 @@ namespace SaY_DeF
 
         private void Button_ChangeNick_Click(object sender, RoutedEventArgs e)
         {
-            if (!String.IsNullOrEmpty(TB_Nick.Text))
-                Settings.myNick = TB_Nick.Text;
+            if (String.IsNullOrEmpty(TB_Nick.Text))
+                return;
+            Settings.myNick = TB_Nick.Text;
+            Task.Run(() => this.Dispatcher.Invoke(new Action(async delegate
+            {
+                Button_ChangeNick.Content = "Confirmed";
+                await Task.Delay(760);
+                Button_ChangeNick.Content = "Confirm";
+            })));
         }
-
-
     }
 }
