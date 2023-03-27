30b4d081b58e3dd78921edfd9c1f9fddd67de71c
SaYMooN <shokoladnysaymoon@mail.ru>
2023-03-27 03:43:17 +0300

    The first prototype of the construction of towers. In the future I will create a separate class for towers

--- Source/Game.cs
+++ Source/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace SaY_DeF.Source
@@ -10,7 +11,7 @@ namespace SaY_DeF.Source
     class Game
     {
         SolidColorBrush brushBack, brushMenu;
-        Canvas gameScreen, BottomMenu;
+        Canvas gameScreen, BottomMenu, fullScreen;
         GameArgs Args;
         Button Btn_GetEnemyScreen;
         ResourceDictionary roundButtons = new ResourceDictionary();
@@ -19,6 +20,7 @@ namespace SaY_DeF.Source
         List<NewTowerTile> ListOfNewTowerTiles = new List<NewTowerTile>();
         Window win;
         Grid TowerGrid;
+        List<Button> attackTowers= new List<Button>();
         Label LB_YourTower;
 
         public Game(GameArgs gArgs, Window w, Net_Connector net)
@@ -39,17 +41,21 @@ namespace SaY_DeF.Source
                 FormTowersGrid();
                 brushBack = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E8E8E8"));
                 brushMenu = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CCCCCC"));
+               
+                fullScreen = new Canvas() { Background = new SolidColorBrush(Colors.Yellow) };
                 gameScreen = new Canvas() { Background = brushBack };
                 BottomMenu = new Canvas() { Background = brushMenu };
+                fullScreen.Children.Add(gameScreen);
                 BottomMenu.Children.Add(TowerGrid);
-                gameScreen.Children.Add(BottomMenu);
+                fullScreen.Children.Add(BottomMenu);
+                
                 
                 Btn_GetEnemyScreen = new Button() { Style = (Style)roundButtons["ButtonStyle"], Content = "Enemy Scren" };
                 Btn_GetEnemyScreen.Click += Btn_GetEnemyScreen_Click;
-                gameScreen.Children.Add(Btn_GetEnemyScreen);
+                fullScreen.Children.Add(Btn_GetEnemyScreen);
                 win.MouseRightButtonDown += Win_MouseRightButtonDown;
                 win.SizeChanged += Win_SizeChanged;
-                win.Content = gameScreen;
+                win.Content = fullScreen;
             }
             );
             MessageBox.Show("Please maximize the window");
@@ -57,8 +63,8 @@ namespace SaY_DeF.Source
 
         private void Win_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
+
             Point position = e.GetPosition(e.Source as UIElement);
-            MessageBox.Show($"X: {position.X}\nY:{position.Y}");
             AddNewTowerTile(position);
         }
 
@@ -81,11 +87,19 @@ namespace SaY_DeF.Source
 
         private void Win_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+
+
+            fullScreen.Width =sWidth;
+            fullScreen.Height= sHeight;
+
             gameScreen.Width = sWidth;
-            gameScreen.Height = sHeight;
-            BottomMenu.Width = gameScreen.Width;
-            BottomMenu.Height = gameScreen.Height * 0.22;
-            Canvas.SetTop(BottomMenu, gameScreen.Height * 0.75);
+            gameScreen.Height = sHeight*0.78;
+            Canvas.SetTop(gameScreen, 0);
+            Canvas.SetLeft(gameScreen, 0);
+
+            BottomMenu.Width = fullScreen.Width;
+            BottomMenu.Height = fullScreen.Height * 0.22;
+            Canvas.SetTop(BottomMenu, fullScreen.Height * 0.78);
 
             Btn_GetEnemyScreen.Height = BottomMenu.Height / 4;
             Btn_GetEnemyScreen.Width = Btn_GetEnemyScreen.Height * 3;
@@ -94,10 +108,10 @@ namespace SaY_DeF.Source
             Canvas.SetBottom(Btn_GetEnemyScreen, Btn_GetEnemyScreen.Height * 1.5);
             Canvas.SetRight(Btn_GetEnemyScreen, Btn_GetEnemyScreen.Height / 2);
 
-            TowerGrid.Height = BottomMenu.Height * 0.75;
+            TowerGrid.Height = BottomMenu.Height * 0.7;
             TowerGrid.Width = TowerGrid.Height * 2;
             Canvas.SetLeft(TowerGrid, (BottomMenu.Width - TowerGrid.Width) / 4);
-            Canvas.SetTop(TowerGrid, (BottomMenu.Height - TowerGrid.Height) / 1.8);
+            Canvas.SetTop(TowerGrid, (BottomMenu.Height - TowerGrid.Height) / 1.9);
         }
         private void AddNewTowerTile(Point pos)
         {
@@ -106,7 +120,7 @@ namespace SaY_DeF.Source
             Button btn = newTowerTile.addBtn;
             
             ListOfNewTowerTiles.Add(newTowerTile);
-            gameScreen.Children.Add(btn);
+            fullScreen.Children.Add(btn);
             pos.X -= btn.Width / 2;
             pos.Y -= btn.Height / 2;
             if (pos.X < 0)
@@ -119,7 +133,7 @@ namespace SaY_DeF.Source
         }
         private void FormTowersGrid()
         {
-            TowerGrid = new Grid() {Background=new SolidColorBrush(Colors.Blue), ShowGridLines=true };
+            TowerGrid = new Grid() {Background=new SolidColorBrush(Colors.LightCoral), ShowGridLines=true };
             for (int i = 0; i < 6; i++)
             {
                 TowerGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
@@ -128,12 +142,35 @@ namespace SaY_DeF.Source
             {
                 TowerGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
             }
+
+
+            var image = new Image()
+            {
+                Source = new BitmapImage(new Uri(@"Towers/GunTower.png", UriKind.Relative)),
+                Stretch = Stretch.Uniform,
+            };
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.NearestNeighbor);
+            Button GunTower = new Button()
+            {
+                Content = image,
+                IsEnabled=false
+            };
+            GunTower.Click += GunTower_Click;
+            attackTowers.Add(GunTower);
+            Grid.SetRow(GunTower, 0);
+            Grid.SetColumn(GunTower, 0);
+            TowerGrid.Children.Add(GunTower);
         }
 
+        private void GunTower_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("GunTower set is building");
+        }
 
         private void BuildTower(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Tower Building");
+            foreach (Button b in attackTowers)
+                b.IsEnabled = true;
         }
     }
 }
